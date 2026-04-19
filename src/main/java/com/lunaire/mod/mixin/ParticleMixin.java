74aba4de6fe253aa3a58484df1f00aa69e9179ea
kerch <kerchtop1258@gmail.com>
2026-04-19 17:18:26 +0300

    Update ParticleMixin.java

--- src/main/java/com/lunaire/mod/mixin/ParticleMixin.java
+++ src/main/java/com/lunaire/mod/mixin/ParticleMixin.java
@@ -1,18 +1,22 @@
 package com.lunaire.mod.mixin;
 
 import com.lunaire.mod.LunaireClient;
+import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.particle.ParticleEffect;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ParticleManager.class)
 public class ParticleMixin {
-    @Inject(method = "addParticle", at = @At("HEAD"), cancellable = true)
-    private void onAddParticle(CallbackInfo ci) {
+    // Используем другой метод впрыска, который реже вызывает краши
+    @Inject(method = "addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)Lnet/minecraft/client/particle/Particle;", at = @At("HEAD"), cancellable = true)
+    private void onAddParticle(ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ, CallbackInfoReturnable<Particle> cir) {
         if (LunaireClient.noRenderParticles) {
-            ci.cancel();
+            cir.setReturnValue(null);
         }
     }
 }
