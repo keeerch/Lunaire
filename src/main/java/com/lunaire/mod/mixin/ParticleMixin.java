bfddd8f3b778e2c45a5c916764987124fdd2f937
kerch <kerchtop1258@gmail.com>
2026-04-19 12:08:59 +0300

    Update ParticleMixin.java

--- src/main/java/com/lunaire/mod/mixin/ParticleMixin.java
+++ src/main/java/com/lunaire/mod/mixin/ParticleMixin.java
@@ -1,18 +1,20 @@
 package com.lunaire.mod.mixin;
 
 import com.lunaire.mod.LunaireClient;
-import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.particle.ParticleEffect;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LightmapTextureManager.class)
-public class LightmapMixin {
-    @Inject(method = "getBrightness", at = @At("HEAD"), cancellable = true)
-    private static void onGetBrightness(CallbackInfoReturnable<Float> cir) {
-        if (LunaireClient.fullBright) {
-            cir.setReturnValue(15.0f); // Максимальная яркость
+@Mixin(ParticleManager.class)
+public class ParticleMixin {
+    @Inject(method = "addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)Lnet/minecraft/client/particle/Particle;", at = @At("HEAD"), cancellable = true)
+    private void onAddParticle(ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ, CallbackInfoReturnable<Particle> cir) {
+        if (LunaireClient.noRenderParticles) {
+            cir.setReturnValue(null);
         }
     }
 }
