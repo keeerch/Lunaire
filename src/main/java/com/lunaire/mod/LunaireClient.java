27bba122fda9360e19bb55ffce385463f4fa3389
kerch <kerchtop1258@gmail.com>
2026-04-19 12:42:04 +0300

    Update LunaireClient.java

--- src/main/java/com/lunaire/mod/LunaireClient.java
+++ src/main/java/com/lunaire/mod/LunaireClient.java
@@ -1,21 +1,14 @@
 package com.lunaire.mod;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.util.math.Vec3d;
 
 public class LunaireClient implements ClientModInitializer {
-    // Включаем визуалы по умолчанию
-    public static boolean enableVisuals = true;
     public static boolean enableNametags = true;
     public static boolean enableNoHurtCam = true;
-    
-    // Переменные для вращения квадрата
-    public static float rotateAngle = 0.0f;
-    public static long lastTickTime = 0;
+    public static boolean fullBright = true;
 
     @Override
     public void onInitializeClient() {
-        System.out.println("Lunaire Visuals Activated!");
-        lastTickTime = System.currentTimeMillis();
+        // Пока оставляем пустым, чтобы ничего не конфликтовало
     }
 }
