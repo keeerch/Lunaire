d73d352693c28abd90289d5cf9fa54be801f01f7
kerch <kerchtop1258@gmail.com>
2026-04-19 13:19:25 +0300

    Update LightmapMixin.java

--- src/main/java/com/lunaire/mod/mixin/LightmapMixin.java
+++ src/main/java/com/lunaire/mod/mixin/LightmapMixin.java
@@ -2,14 +2,8 @@ package com.lunaire.mod.mixin;
 
 import net.minecraft.client.render.LightmapTextureManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = LightmapTextureManager.class, priority = 10000)
+@Mixin(LightmapTextureManager.class)
 public class LightmapMixin {
-    @Inject(method = "getBrightness", at = @At("HEAD"), cancellable = true)
-    private static void onGetBrightness(CallbackInfoReturnable<Float> cir) {
-        cir.setReturnValue(15.0f);
-    }
+    // Удалили инъекцию, которая вешала загрузку на 70%
 }
