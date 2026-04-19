6a7fa14c5274d7e6558bdbeddcb843f4e62e02b6
kerch <kerchtop1258@gmail.com>
2026-04-19 12:42:55 +0300

    Update GameRendererMixin.java

--- src/main/java/com/lunaire/mod/mixin/GameRendererMixin.java
+++ src/main/java/com/lunaire/mod/mixin/GameRendererMixin.java
@@ -2,6 +2,7 @@ package com.lunaire.mod.mixin;
 
 import com.lunaire.mod.LunaireClient;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
     @Inject(method = "bobView", at = @At("HEAD"), cancellable = true)
-    private void onBobView(CallbackInfo ci) {
+    private void onBobView(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
         if (LunaireClient.enableNoHurtCam) {
             ci.cancel();
         }
