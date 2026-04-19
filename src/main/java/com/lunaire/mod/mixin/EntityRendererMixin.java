0f8cb0db2c84b4433bd14b08196586ac90560c63
kerch <kerchtop1258@gmail.com>
2026-04-19 17:14:28 +0300

    Update EntityRendererMixin.java

--- src/main/java/com/lunaire/mod/mixin/EntityRendererMixin.java
+++ src/main/java/com/lunaire/mod/mixin/EntityRendererMixin.java
@@ -3,17 +3,20 @@ package com.lunaire.mod.mixin;
 import com.lunaire.mod.LunaireClient;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(EntityRenderer.class)
-public abstract class EntityRendererMixin {
-    @Inject(method = "hasLabel", at = @At("HEAD"), cancellable = true)
-    private void onHasLabel(Entity entity, CallbackInfoReturnable<Boolean> cir) {
-        if (LunaireClient.enableNametags) {
-            cir.setReturnValue(true);
+public class EntityRendererMixin {
+    @ModifyArgs(method = "renderLabelIfPresent", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/Text;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I"))
+    private void onDrawLabel(Args args, Entity entity) {
+        if (LunaireClient.enableNametags && entity instanceof PlayerEntity) {
+            // Заменяем цвет текста (аргумент №3 в методе draw) на наш акцентный цвет
+            args.set(3, LunaireClient.accentColor);
         }
     }
 }
