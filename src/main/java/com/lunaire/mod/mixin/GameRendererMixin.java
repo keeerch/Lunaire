4be7dd6fedf4c8efc9025286f6013b7260dafb73
kerch <kerchtop1258@gmail.com>
2026-04-19 17:26:19 +0300

    Update GameRendererMixin.java

--- src/main/java/com/lunaire/mod/mixin/GameRendererMixin.java
+++ src/main/java/com/lunaire/mod/mixin/GameRendererMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = GameRenderer.class, priority = 1001) // Приоритет выше, чтобы перебить другие моды
+@Mixin(value = GameRenderer.class) // Приоритет выше, чтобы перебить другие моды
 public class GameRendererMixin {
     @Inject(method = "tiltViewWhenHurt", at = @At("HEAD"), cancellable = true)
     private void onTiltViewWhenHurt(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
