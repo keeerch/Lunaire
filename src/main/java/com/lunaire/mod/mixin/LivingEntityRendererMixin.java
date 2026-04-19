d2658148fe21be50fb57e4f43dd72d9c65f296a9
kerch <kerchtop1258@gmail.com>
2026-04-19 12:06:44 +0300

    Create LivingEntityRendererMixin.java

---
+++ src/main/java/com/lunaire/mod/mixin/LivingEntityRendererMixin.java
@@ -0,0 +1,16 @@
+package com.lunaire.mod.mixin;
+
+import com.lunaire.mod.LunaireClient;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(LivingEntityRenderer.class)
+public class LivingEntityRendererMixin {
+    @ModifyArgs(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;getRenderLayer(Lnet/minecraft/entity/LivingEntity;ZZZ)Lnet/minecraft/client/render/RenderLayer;"))
+    private void changeHitColor(Args args) {
+        // Здесь можно будет внедрить логику смены цвета через LunaireClient.hitColor
+    }
+}
