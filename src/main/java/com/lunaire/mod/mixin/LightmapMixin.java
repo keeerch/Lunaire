1803f00cb20a79548461a4ca1f2496faacbb2c92
kerch <kerchtop1258@gmail.com>
2026-04-19 13:36:52 +0300

    Update LightmapMixin.java

--- src/main/java/com/lunaire/mod/mixin/LightmapMixin.java
+++ src/main/java/com/lunaire/mod/mixin/LightmapMixin.java
@@ -1,9 +1,18 @@
 package com.lunaire.mod.mixin;
 
+import com.lunaire.mod.LunaireClient;
 import net.minecraft.client.render.LightmapTextureManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LightmapTextureManager.class)
 public class LightmapMixin {
-    // Пусто. Совсем пусто.
+    @Inject(method = "getBrightness", at = @At("HEAD"), cancellable = true)
+    private void onGetBrightness(CallbackInfoReturnable<Float> cir) {
+        if (LunaireClient.fullBright) {
+            cir.setReturnValue(15.0f);
+        }
+    }
 }
