70f72520532a22777e983d7d20380a6379492054
kerch <kerchtop1258@gmail.com>
2026-04-19 13:36:00 +0300

    Update LunaireClient.java

--- src/main/java/com/lunaire/mod/LunaireClient.java
+++ src/main/java/com/lunaire/mod/LunaireClient.java
@@ -1,16 +1,26 @@
 package com.lunaire.mod;
 
+import com.lunaire.mod.gui.ClickGuiScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class LunaireClient implements ClientModInitializer {
-    // ТЕ САМЫЕ ПЕРЕМЕННЫЕ, КОТОРЫЕ ИЩЕТ КОМПИЛЯТОР:
-    public static boolean noRenderParticles = true;
     public static boolean enableNametags = true;
     public static boolean enableNoHurtCam = true;
     public static boolean fullBright = true;
+    public static boolean noRenderParticles = true;
 
     @Override
     public void onInitializeClient() {
-        System.out.println("Lunaire Client Loaded!");
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            // Проверяем нажатие Правого Шифта (GLFW_KEY_RIGHT_SHIFT)
+            if (client.player != null && InputUtil.isKeyPressed(client.getWindow().getHandle(), GLFW.GLFW_KEY_RIGHT_SHIFT)) {
+                if (!(client.currentScreen instanceof ClickGuiScreen)) {
+                    client.setScreen(new ClickGuiScreen());
+                }
+            }
+        });
     }
 }
