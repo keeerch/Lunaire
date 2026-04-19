25c6e1adc9a8c59ab4f7a0794b44439fec9cb0d3
kerch <kerchtop1258@gmail.com>
2026-04-19 17:14:13 +0300

    Update LightmapMixin.java

--- src/main/java/com/lunaire/mod/mixin/LightmapMixin.java
+++ src/main/java/com/lunaire/mod/mixin/LightmapMixin.java
@@ -7,12 +7,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LightmapTextureManager.class)
+@Mixin(value = LightmapTextureManager.class, priority = 1001)
 public class LightmapMixin {
     @Inject(method = "getBrightness", at = @At("HEAD"), cancellable = true)
     private static void onGetBrightness(CallbackInfoReturnable<Float> cir) {
         if (LunaireClient.fullBright) {
-            cir.setReturnValue(15.0f); // Ставим свет на максимум
+            cir.setReturnValue(100.0f); // Поставил 100 вместо 15, чтобы уж точно светило
         }
     }
 }
