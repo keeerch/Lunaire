40d90bc6b8f6346a88a211ea5a3407d64a0113a9
kerch <kerchtop1258@gmail.com>
2026-04-19 11:24:13 +0300

    Create ParticleMixin.java

---
+++ src/main/java/com/lunaire/mod/mixin/ParticleMixin.java
@@ -0,0 +1,20 @@
+package com.lunaire.mod.mixin;
+
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.particle.ParticleEffect;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ParticleManager.class)
+public class ParticleMixin {
+    @Inject(method = "addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)Lnet/minecraft/client/particle/Particle;", at = @At("HEAD"), cancellable = true)
+    private void onAddParticle(ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ, CallbackInfoReturnable<Particle> cir) {
+        // Если функция включена — просто отменяем создание частицы
+        if (LunaireConfig.noRenderParticles) {
+            cir.setReturnValue(null);
+        }
+    }
+}
