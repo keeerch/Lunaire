4cfe2b8fa99f3a4eff9bdee2c973d32e7dd9ab61
kerch <kerchtop1258@gmail.com>
2026-04-19 16:41:37 +0300

    Update ParticleMixin.java

--- src/main/java/com/lunaire/mod/mixin/ParticleMixin.java
+++ src/main/java/com/lunaire/mod/mixin/ParticleMixin.java
@@ -1,20 +1,18 @@
 package com.lunaire.mod.mixin;
 
 import com.lunaire.mod.LunaireClient;
-import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleManager;
-import net.minecraft.particle.ParticleEffect;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ParticleManager.class)
 public class ParticleMixin {
-    @Inject(method = "addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)Lnet/minecraft/client/particle/Particle;", at = @At("HEAD"), cancellable = true)
-    private void onAddParticle(ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ, CallbackInfoReturnable<Particle> cir) {
+    @Inject(method = "addParticle", at = @At("HEAD"), cancellable = true)
+    private void onAddParticle(CallbackInfo ci) {
         if (LunaireClient.noRenderParticles) {
-            cir.setReturnValue(null);
+            ci.cancel();
         }
     }
 }
