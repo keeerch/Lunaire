f30b88bef2c9bfa9e651d20e681cf3b7bf676f18
kerch <kerchtop1258@gmail.com>
2026-04-19 12:48:21 +0300

    Update LunaireClient.java

--- src/main/java/com/lunaire/mod/LunaireClient.java
+++ src/main/java/com/lunaire/mod/LunaireClient.java
@@ -3,12 +3,14 @@ package com.lunaire.mod;
 import net.fabricmc.api.ClientModInitializer;
 
 public class LunaireClient implements ClientModInitializer {
+    // ТЕ САМЫЕ ПЕРЕМЕННЫЕ, КОТОРЫЕ ИЩЕТ КОМПИЛЯТОР:
+    public static boolean noRenderParticles = true;
     public static boolean enableNametags = true;
     public static boolean enableNoHurtCam = true;
     public static boolean fullBright = true;
 
     @Override
     public void onInitializeClient() {
-        // Пока оставляем пустым, чтобы ничего не конфликтовало
+        System.out.println("Lunaire Client Loaded!");
     }
 }
