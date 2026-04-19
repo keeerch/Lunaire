6b068fe2987f34e420236b648c46ec4f6bfd08c8
kerch <kerchtop1258@gmail.com>
2026-04-19 12:40:38 +0300

    Update EntityRendererMixin.java

--- src/main/java/com/lunaire/mod/mixin/EntityRendererMixin.java
+++ src/main/java/com/lunaire/mod/mixin/EntityRendererMixin.java
@@ -3,19 +3,16 @@ package com.lunaire.mod.mixin;
 import com.lunaire.mod.LunaireClient;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityRenderer.class)
-public class EntityRendererMixin {
+public abstract class EntityRendererMixin {
     @Inject(method = "hasLabel", at = @At("HEAD"), cancellable = true)
     private void onHasLabel(Entity entity, CallbackInfoReturnable<Boolean> cir) {
-        // Если это игрок и Наметаги включены
-        if (entity instanceof PlayerEntity && LunaireClient.enableNametags) {
-            // Заставляем игру думать, что у него всегда есть ник
+        if (LunaireClient.enableNametags) {
             cir.setReturnValue(true);
         }
     }
